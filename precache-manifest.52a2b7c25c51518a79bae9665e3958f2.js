72f50a85c493a6c01d51d17b7f180aa58f04e5a7
John <11955347+JohnSongSoftware@users.noreply.github.com>
2019-12-19 21:25:27 -0500

    Updates

---
+++ precache-manifest.52a2b7c25c51518a79bae9665e3958f2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e21a3adffb2d3d2f5770b41d65af39",
+    "url": "/ctrlfyp-website/index.html"
+  },
+  {
+    "revision": "7ed7a6d465487d9794f3",
+    "url": "/ctrlfyp-website/static/css/main.c92488c9.chunk.css"
+  },
+  {
+    "revision": "67c7b24feb056549ef5e",
+    "url": "/ctrlfyp-website/static/js/2.2ffdf9ff.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/ctrlfyp-website/static/js/2.2ffdf9ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ed7a6d465487d9794f3",
+    "url": "/ctrlfyp-website/static/js/main.7cc3ebec.chunk.js"
+  },
+  {
+    "revision": "6980c00c1792878864a0",
+    "url": "/ctrlfyp-website/static/js/runtime-main.58b4d51c.js"
+  },
+  {
+    "revision": "23cf82ba04680b72d5616d74aa3f5550",
+    "url": "/ctrlfyp-website/static/media/404-picture.23cf82ba.jpg"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/ctrlfyp-website/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "c67b468dee946ac8db3d39e3ee47cd5b",
+    "url": "/ctrlfyp-website/static/media/ctrlfyp-logo.c67b468d.png"
+  },
+  {
+    "revision": "970f2bb77e577c9b23ae51eb214add94",
+    "url": "/ctrlfyp-website/static/media/esrb-rp-logo.970f2bb7.jpg"
+  },
+  {
+    "revision": "015d2a31e9c355fa3f86d36f5828f439",
+    "url": "/ctrlfyp-website/static/media/header.015d2a31.png"
+  },
+  {
+    "revision": "a46fb21b8c09f95678544f7dbd746f62",
+    "url": "/ctrlfyp-website/static/media/test-image-001.a46fb21b.png"
+  }
+]);
